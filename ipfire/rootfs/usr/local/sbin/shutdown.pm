67543fb7e17a729d8a2549c07280c0ff2e896f8f
root <root@ipfire.jbsky.fr>
2016-10-29 16:56:42 +0200

    modified:   ipfire/rootfs/etc/default/bridge
    	new file:   ipfire/rootfs/etc/default/vhost
    	=> possibilité de mettre des variables par defaut
    
    	modified:   ipfire/rootfs/etc/init.d/bridge
    
    	modified:   ipfire/rootfs/etc/init.d/vhost
    	=> add virtio driver for image disk
    	=> complete refonte du script
    
    	new file:   ipfire/rootfs/usr/local/sbin/shutdown.pm
    	=> shutdown a VM now work every time.

---
+++ ipfire/rootfs/usr/local/sbin/shutdown.pm
@@ -0,0 +1,32 @@
+#!/usr/bin/perl -w
+
+use strict;
+use warnings;
+use IO::Socket::UNIX qw(SOCK_STREAM);
+
+my $RUN_PATH="/var/run";
+
+my $sendMonitor = sub {
+    my $name = shift;
+    my $msg  = shift;
+    print "$RUN_PATH/$name $msg";
+
+    my $sock = IO::Socket::UNIX->new(
+        Type => SOCK_STREAM,
+        Peer => "$RUN_PATH/$name",
+    ) || do {
+        print "Cannot open socket $!\n";
+        exit 1;
+    };
+    print $sock $msg;
+	# print $sock "$msg\n";
+    $sock->close();
+};
+
+my $acpiShutdown = sub {
+    $sendMonitor->(shift, "system_powerdown\n");
+};
+
+$acpiShutdown->($ARGV[0]);
+
+1;
